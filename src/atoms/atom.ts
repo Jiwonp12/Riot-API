fc672d6f0cce8bcc37da7317c6300b19378e7404
Jiwonp12 <ndwoo33@gmail.com>
2024-02-29 13:25:04 +0900

    feat: recoil persist

--- src/atoms/atom.ts
+++ src/atoms/atom.ts
@@ -1,17 +1,23 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 import { Rune, Spell } from "../types/types";
+
+const { persistAtom } = recoilPersist();
 
 export const spellAtom = atom<Spell[]>({
   key: "spellAtom",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const runeAtom = atom<Rune[]>({
   key: "runeAtom",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const summonerAtom = atom({
   key: "summonerAtom",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
