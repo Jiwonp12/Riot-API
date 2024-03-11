ccde37cf27f3a4f9449697994155dca19020e68d
Jiwonp12 <ndwoo33@gmail.com>
2024-03-11 13:12:58 +0900

    feat: champions 페이지 추가

---
+++ src/pages/Champions.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function Champions() {
+  return <S_Main></S_Main>;
+}
+
+export default Champions;
+
+const S_Main = styled.main`
+  display: flex;
+  padding: 20px 100px;
+`;
